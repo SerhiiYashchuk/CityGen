d143464881ca253c5b4ae6a26dfc15efdbf1bbd9
mandarinus9 <mandarinus9@gmail.com>
2018-11-19 01:45:28 +0200

    Added tests for vectors
    Added tests for next some vector functions

--- test/GenericTypesTests.cpp
+++ test/GenericTypesTests.cpp
@@ -3,7 +3,10 @@
 #include "GenericTypes.h"
 #include "catch.hpp"
 
-
+inline float vectorLength(float x, float y)
+{
+	return std::sqrt(x * x + y * y);
+}
 
 TEST_CASE("Vector lenght", "[vector]")
 {
@@ -32,6 +35,21 @@ TEST_CASE("Vector lenght", "[vector]")
 
 		REQUIRE(vector.length() > 0);
 	}
+
+  SECTION("Coordinates [i,j] length")
+  {
+    for (int i = -5; i < 5; i++)
+    {
+      vector.x = i;
+      for (int j = -5; j < 5; j++)
+      {
+        vector.y = j;
+
+        REQUIRE(vector.length() == vectorLength(i, j));
+	  }
+    }
+
+  }
 }
 
 TEST_CASE("Vectors equality", "[vector]")
@@ -243,3 +261,245 @@ TEST_CASE("Vectors coordinates multiplication", "[vector]")
 		REQUIRE(tmp.y == 1);
 	}
 }
+
+TEST_CASE("Vectors coordinates division", "[vector]")
+{
+	CityGen::Vector A;
+	CityGen::Vector B;
+
+	SECTION("[0,0] / [1,1] must be = [0,0]")
+	{
+		A.x = A.y = 0;
+		B.x = B.y = 1;
+
+		CityGen::Vector tmp = A / B;
+		REQUIRE(tmp.x == 0);
+		REQUIRE(tmp.y == 0);
+	}
+
+	SECTION("[1,1] / [1,1] must be = [1,1]")
+	{
+		A.x = A.y = 1;
+		B.x = B.y = 1;
+
+		CityGen::Vector tmp = A / B;
+		REQUIRE(tmp.x == 1);
+		REQUIRE(tmp.y == 1);
+	}
+}
+
+TEST_CASE("Vectors coordinate += operation", "[vector]")
+{
+  CityGen::Vector A;
+  CityGen::Vector B;
+
+  SECTION("[0,0] += [0,0] must be = [0,0]")
+  {
+	  A.x = A.y = 0;
+	  B.x = B.y = 0;
+
+	  A += B;
+	  REQUIRE(A.x == 0);
+	  REQUIRE(A.y == 0);
+  }
+
+  SECTION("[0,0] += [1,1] must be = [1,1]")
+  {
+	  A.x = A.y = 0;
+	  B.x = B.y = 1;
+
+	  A += B;
+
+	  REQUIRE(A.x == 1);
+	  REQUIRE(A.y == 1);
+  }
+}
+
+TEST_CASE("Vectors coordinate -= operation", "[vector]")
+{
+  CityGen::Vector A;
+  CityGen::Vector B;
+
+  SECTION("[0,0] -= [0,0] must be = [0,0]")
+  {
+	  A.x = A.y = 0;
+	  B.x = B.y = 0;
+
+	  A -= B;
+
+	  REQUIRE(A.x == 0);
+	  REQUIRE(A.y == 0);
+  }
+
+  SECTION("[0,0] -= [1,1] must be = [-1,-1]")
+  {
+	  A.x = A.y = 0;
+	  B.x = B.y = 1;
+
+	  A -= B;
+
+	  REQUIRE(A.x == -1);
+	  REQUIRE(A.y == -1);
+  }
+
+  SECTION("[1,1] -= [1,1] must be = [0,0]")
+  {
+	  A.x = A.y = 1;
+	  B.x = B.y = 1;
+
+	  A -= B;
+
+	  REQUIRE(A.x == 0);
+	  REQUIRE(A.y == 0);
+  }
+}
+
+TEST_CASE("Vectors coordinate *= operation", "[vector]")
+{
+  CityGen::Vector A;
+  CityGen::Vector B;
+
+  SECTION("[0,0] *= [0,0] must be = [0,0]")
+  {
+	  A.x = A.y = 0;
+	  B.x = B.y = 0;
+
+	  A *= B;
+
+	  REQUIRE(A.x == 0);
+	  REQUIRE(A.y == 0);
+  }
+
+  SECTION("[0,0] *= [1,1] must be = [0,0]")
+  {
+	  A.x = A.y = 0;
+	  B.x = B.y = 1;
+
+	  A *= B;
+
+	  REQUIRE(A.x == 0);
+	  REQUIRE(A.y == 0);
+  }
+
+  SECTION("[1,1] *= [1,1] must be = [1,1]")
+  {
+	  A.x = A.y = 1;
+	  B.x = B.y = 1;
+
+	  A *= B;
+
+	  REQUIRE(A.x == 1);
+	  REQUIRE(A.y == 1);
+  }
+}
+
+TEST_CASE("Vectors coordinate /= operation", "[vector]")
+{
+  CityGen::Vector A;
+  CityGen::Vector B;
+
+  SECTION("[0,0] /= [1,1] must be = [0,0]")
+  {
+	  A.x = A.y = 0;
+	  B.x = B.y = 1;
+
+	  A /= B;
+
+	  REQUIRE(A.x == 0);
+	  REQUIRE(A.y == 0);
+  }
+
+  SECTION("[1,1] /= [1,1] must be = [1,1]")
+  {
+	  A.x = A.y = 1;
+	  B.x = B.y = 1;
+
+	  A /= B;
+
+	  REQUIRE(A.x == 1);
+	  REQUIRE(A.y == 1);
+  }
+}
+
+TEST_CASE("Vector scalar multiplacation", "[vector]")
+{
+  CityGen::Vector vector;
+
+  SECTION("[0,0] * 0 must be = [0,0]")
+  {
+	  vector.x = vector.y = 0;
+
+	  vector = vector * 0;
+
+	  CityGen::Vector tmp;
+	  tmp.x = tmp.y = 0;
+
+	  REQUIRE(vector == tmp);
+  }
+
+  SECTION("[0,0] * 1 must be = [0,0]")
+  {
+	  vector.x = vector.y = 0;
+
+	  vector = vector * 1;
+
+	  CityGen::Vector tmp;
+	  tmp.x = tmp.y = 0;
+
+	  REQUIRE(vector == tmp);
+  }
+
+  SECTION("[1,1] * 0 must be = [0,0]")
+  {
+    vector.x = vector.y = 1;
+
+	vector = vector * 0;
+
+	CityGen::Vector tmp;
+    tmp.x = tmp.y = 0;
+
+	REQUIRE(vector == tmp);
+  }
+
+  SECTION("[1,1] * 1 must be = [1,1]")
+  {
+	  vector.x = vector.y = 1;
+
+	  vector = vector * 1;
+
+	  CityGen::Vector tmp;
+	  tmp.x = tmp.y = 1;
+
+	  REQUIRE(vector == tmp);
+  }
+}
+
+TEST_CASE("Vector scalar division", "[vector]")
+{
+  CityGen::Vector vector;
+
+  SECTION("[0,0] / 1 must be = [0,0]")
+  {
+    vector.x = vector.y = 0;
+
+    vector = vector / 1;
+
+    CityGen::Vector tmp;
+	tmp.x = tmp.y = 0;
+
+	REQUIRE(vector == tmp);
+  }
+
+  SECTION("[1,1] / 1 must be = [1,1]")
+  {
+	  vector.x = vector.y = 1;
+
+	  vector = vector / 1;
+
+	  CityGen::Vector tmp;
+	  tmp.x = tmp.y = 1;
+
+	  REQUIRE(vector == tmp);
+  }
+}
+
